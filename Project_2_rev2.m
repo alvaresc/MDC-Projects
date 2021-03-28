6fc08961089883283a18eab3fe97d4bda4074f31
alvaresc <48186019+alvaresc@users.noreply.github.com>
2021-03-28 17:36:58 -0400

    Create Project_2_rev2.m

---
+++ Project_2_rev2.m
@@ -0,0 +1,171 @@
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%% 
+% Ian Landwehr, Sam Alvares, and Sam Ridgley
+% March 30rd, 2021
+%
+% ME480: Machine Component Design
+% Dr. Constans
+% Spring 2021
+%
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+
+%% Basic Level 
+clc
+close all
+clear variables
+
+% Geometry
+Aloc = [1 -24 36];
+COMloc = [38 -36 36];
+MomentArm = COMloc-Aloc;
+
+% Reaction At A
+ExtForces = [0 30 0]; % Shear Force on A
+ExtMoment = cross(MomentArm, ExtForces);
+
+IntForces = -1*ExtForces;
+IntMoment = -1*ExtMoment;
+
+
+% Beam Shear due to Fy
+T = 1/4 ; %     The cross sectional wall thickness area. 
+Do = 1 ;%       The outer Diameter of the lower pole
+Di = 1-1/4;%    The inner Diameter of the lower pole
+alpha = 90; %   Degrees (half-circle)
+Q = 2*sind(alpha)/3*((Do/2)^3-(Di/2)^3); 
+I = pi/64*(Do^4-Di^4);
+V = IntForces(2);
+Tau = V * Q /I/T; 
+
+% Bending Moment due to Mz
+Mz = IntMoment(3);
+sigma = IntMoment * Do / 2 / I ;
+
+%% Medium Level
+clc
+close all
+clear variables
+
+% Geometry
+Bloc = [0 -180 1];
+COMloc = [38 -36 36];
+MomentArm = COMloc-Bloc;
+
+% Reaction At B
+ExtForces = [0 30 0]; % Shear Force on B
+ExtMoment = cross(MomentArm, ExtForces);
+IntForces = -1*ExtForces
+IntMoment = -1*ExtMoment
+
+
+% Beam Shear due to Fy
+Do = 2 ;%       The outer Diameter of the lower pole
+Di = 1.5;%    The inner Diameter of the lower pole
+alpha = 90; %   Degrees (half-circle)
+Q = 2*sind(alpha)/3*((Do/2)^3-(Di/2)^3); 
+I = pi/64*(Do^4-Di^4);
+T = 0.5;
+V = IntForces(2);
+TauBeam = V * Q /I/T;
+
+% Torsion due to Mz
+T = IntMoment(3);
+J = pi/32*(Do^4-Di^4);
+TauTorsion = (T*Do)/(2*J);
+
+% Bending Moment due to Mx
+Mx = IntMoment(1);
+sigmaMx = Mx * Do / 2 / I ;
+
+% Enter stress matricies
+sigma1 = [         0, 0,TauTorsion;
+                   0, 0,         0;
+          TauTorsion, 0,   sigmaMx];
+      
+sigma2 = [0,                     0,                     0;
+          0,                     0, (TauTorsion+TauBeam);
+          0, (TauTorsion+TauBeam),                     0];
+
+sigma3 = [         0, 0,TauTorsion;
+                   0, 0,          0;
+          TauTorsion, 0,  sigmaMx];
+      
+sigma4 = [0,                     0,                    0;
+          0,                     0, (TauTorsion+TauBeam);
+          0,  (TauTorsion+TauBeam),                    0];
+      
+% Find Principle stresses
+[V1,D1] = eigs(sigma1);
+[V2,D2] = eigs(sigma2);
+[V3,D3] = eigs(sigma3);
+[V4,D4] = eigs(sigma4);
+%% Advanced
+clc
+clear all
+close all
+clc
+close all
+clear variables
+
+% Geometry
+Bloc = [0 -180 1];
+COMloc = [38 -36 36];
+MomentArm = Bloc-COMloc;
+
+% Find External Moments/Forces
+ForceVector = 15*[2 1 3]*1/3.7417;
+ExtForces = [0 -30 0]+ForceVector;
+ % Shear Force on A
+ExtMoment = cross(MomentArm, ExtForces);
+
+% Beam Shear due to Fy
+Do = 2 ;%       The outer Diameter of the lower pole
+Di = 1.5;%    The inner Diameter of the lower pole
+alpha = 90; %   Degrees (half-circle)
+Q = 2*sind(alpha)/3*((Do/2)^3-(Di/2)^3); 
+I = pi/64*(Do^4-Di^4);
+T = 0.5;
+V = abs(ExtForces(2));
+TauBeam = V * Q /I/T;
+
+% Torsion due to Mz
+T = abs(ExtMoment(3));
+J = pi/32*(Do^4-Di^4);
+TauTorsion = (T*Do)/(2*J);
+
+% Bending Moment due to Mx
+Mx = abs(ExtMoment(1));
+sigmaMx = Mx * Do / 2 / I ;
+% Bending Moment due to My
+My = abs(ExtMoment(2));
+sigmaMy = My * Do / 2 / I ;
+
+% Enter stress matricies
+sigma1 = [         0, 0,TauTorsion;
+                   0, 0,         0;
+          TauTorsion, 0,   sigmaMx];
+      
+sigma2 = [0,                     0,                     0;
+          0,                     0, -(TauTorsion-TauBeam);
+          0, -(TauTorsion-TauBeam),                     0];
+
+sigma3 = [         0, 0,-TauTorsion;
+                   0, 0,          0;
+          -TauTorsion, 0,  -sigmaMx];
+      
+sigma4 = [0,                     0,                    0;
+          0,                     0, (TauTorsion+TauBeam);
+          0,  (TauTorsion+TauBeam),                    0];
+      
+% Find Principle stresses
+[V1,D1] = eigs(sigma1);
+[V2,D2] = eigs(sigma2);
+[V3,D3] = eigs(sigma3);
+[V4,D4] = eigs(sigma4);
+
+
+
+
+
+
+
+
